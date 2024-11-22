badbf31132094c7653f3b3b1ef9090d6b773a2dc
langeflx7 <langeflx7@gmail.com>
2024-11-22 11:20:49 +0100

    added settingshandler

---
+++ utils/SettingsHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveGameForKids.utils
+{
+    class SettingsHandler
+    {
+
+        // Singleton instance of SettingsHandler
+        private static SettingsHandler _instance;
+
+        // Path to the file where settings are stored as XML
+        private const string FILE_PATH = "settings.xml";
+
+        /// <summary>
+        /// Private constructor to prevent external instantiation and enforce singleton pattern.
+        /// </summary>
+        private SettingsHandler() { }
+
+        /// <summary>
+        /// Gets the round duration time in seconds
+        /// </summary>
+        /// <returns>The round duration time as integer in seconds</returns>
+        public int GetRoundDurationInSeconds()
+        {
+            return -1; // todo: implement content to method
+        }
+
+        /// <summary>
+        /// Provides a singleton instance of the SettingsHandler class.
+        /// </summary>
+        /// <returns>The singleton instance of SettingsHandler.</returns>
+        public static SettingsHandler GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SettingsHandler();
+            }
+            return _instance;
+        }
+
+    }
+}
